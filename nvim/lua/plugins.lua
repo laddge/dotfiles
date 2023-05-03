3243d19d6f8d439997e1cffe2ac6f5964582a6c9
Laddge <dev.laddge@gmail.com>
2023-05-03 05:05:12 +0000

    optimize nvim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,16 +1,56 @@
 vim.cmd('packadd vim-jetpack')
 require('jetpack.packer').startup(function(use)
   use { 'tani/vim-jetpack', opt = 1 }
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'ray-x/cmp-treesitter'
+  use {
+    'williamboman/mason.nvim',
+    event = { 'VimEnter' },
+    requires = {
+      { 'neovim/nvim-lspconfig', event = { 'VimEnter' }, after = 'mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim', event = { 'VimEnter' }, after = 'mason.nvim' },
+    },
+    config = function()
+      require('mason').setup()
+      require('mason-lspconfig').setup_handlers({ function(server)
+        require('lspconfig')[server].setup({})
+      end })
+    end,
+  }
+  use {
+    'hrsh7th/nvim-cmp',
+    event = { 'InsertEnter' },
+    requires = {
+      { 'hrsh7th/cmp-nvim-lsp', event = { 'InsertEnter' } },
+      { 'hrsh7th/cmp-buffer', event = { 'InsertEnter' } },
+      { 'hrsh7th/cmp-path', event = { 'InsertEnter' } },
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+          end,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ['<C-l>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        experimental = {
+          ghost_text = true,
+        },
+      })
+    end,
+  }
   use { 'vim-skk/eskk.vim',
+    event = { 'VimEnter' },
     config = function()
       vim.g['eskk#directory'] = '~/.eskk'
       vim.g['eskk#dictionary'] = { path = '~/.eskk_jisyo', sorted = 1, encoding = 'utf-8' }
@@ -18,16 +58,18 @@ require('jetpack.packer').startup(function(use)
     end,
   }
   use { 'wuelnerdotexe/vim-astro',
+    ft = { 'astro' },
     config = function()
       vim.g.astro_typescript = 'enable'
     end,
   }
-  use 'evanleck/vim-svelte'
+  use { 'evanleck/vim-svelte', ft = { 'svelte' } }
   use { 'nvim-lualine/lualine.nvim',
+    event = { 'VimEnter' },
     config = function()
-      vim.fn.setcellwidths({
-        { 0xe0b0, 0xe0b3, 1 },
-      })
+      local cellwidths = vim.fn.getcellwidths()
+      table.insert(cellwidths, { 0xe0b0, 0xe0b3, 1 })
+      vim.fn.setcellwidths(cellwidths)
       local function skkmode()
         if vim.call('eskk#is_enabled') == 1 then
           return vim.g['eskk#statusline_mode_strings'][vim.call('eskk#get_mode')]
@@ -62,20 +104,23 @@ require('jetpack.packer').startup(function(use)
   }
   use 'lambdalisue/fern-hijack.vim'
   use { 'lewis6991/gitsigns.nvim',
+    event = { 'VimEnter' },
     config = function()
       vim.opt.signcolumn = 'yes'
-      vim.fn.setcellwidths({
-        { 0x2500, 0x257f, 1 },
-      })
+      local cellwidths = vim.fn.getcellwidths()
+      table.insert(cellwidths, { 0x2500, 0x257f, 1 })
+      vim.fn.setcellwidths(cellwidths)
       require('gitsigns').setup()
     end,
   }
   use { 'terrortylor/nvim-comment',
+    event = { 'VimEnter' },
     config = function()
       require('nvim_comment').setup()
     end,
   }
   use { 'lukas-reineke/indent-blankline.nvim',
+    event = { 'VimEnter' },
     config = function()
       require('indent_blankline').setup({
         space_char_blankline = ' ',
@@ -83,13 +128,15 @@ require('jetpack.packer').startup(function(use)
       vim.g.indent_blankline_char = '|'
     end,
   }
-  use 'lukoshkin/trailing-whitespace'
+  use { 'lukoshkin/trailing-whitespace', event = { 'VimEnter' } }
   use { 'kylechui/nvim-surround',
+    event = { 'VimEnter' },
     config = function()
       require('nvim-surround').setup()
     end,
   }
   use { 'nvim-treesitter/nvim-treesitter',
+    event = { 'VimEnter' },
     run = ':TSUpdate',
     config = function()
       require'nvim-treesitter.configs'.setup({
@@ -98,6 +145,7 @@ require('jetpack.packer').startup(function(use)
     end,
   }
   use { 'catppuccin/nvim',
+    event = { 'VimEnter' },
     as = 'catppuccin',
     config = function()
       require('catppuccin').setup({
@@ -107,5 +155,3 @@ require('jetpack.packer').startup(function(use)
     end,
   }
 end)
-
-require('lsp')
