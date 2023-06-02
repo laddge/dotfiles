5de27b32f170e91c97c670fb0e49cc97adc0ac00
Laddge <dev.laddge@gmail.com>
2023-06-02 08:35:07 +0000

    add nvim tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -111,12 +111,79 @@ require('jetpack.packer').startup(function(use)
       })
     end,
   }
-  use { 'lambdalisue/fern.vim',
+  use { 'nvim-tree/nvim-tree.lua',
+    event = { 'VimEnter' },
+    requires = {
+      { 'nvim-tree/nvim-web-devicons', event = { 'VimEnter' } },
+    },
     config = function()
-      vim.api.nvim_set_keymap('n', '<leader>e', ':Fern . -reveal=% -drawer -toggle<CR>', { noremap = true, silent = true })
+      require('nvim-web-devicons').setup({
+        override = {
+          ts = {
+            icon = '',
+            color = "#519aba",
+            cterm_color = "74",
+            name = "Ts",
+          },
+          cjs = {
+            icon = '󰌞',
+            color = "#f1e05a",
+            cterm_color = "185",
+            name = "Cjs",
+          },
+          js = {
+            icon = '󰌞',
+            color = "#f1e05a",
+            cterm_color = "185",
+            name = "Js",
+          },
+          mjs = {
+            icon = '󰌞',
+            color = "#f1e05a",
+            cterm_color = "185",
+            name = "Mjs",
+          },
+        },
+      })
+      local HEIGHT_RATIO = 0.5
+      local WIDTH_RATIO = 0.5
+      require('nvim-tree').setup({
+        disable_netrw = true,
+        hijack_cursor = true,
+        hijack_netrw = true,
+        respect_buf_cwd = true,
+        sync_root_with_cwd = true,
+        view = {
+          float = {
+            enable = true,
+            open_win_config = function()
+              local screen_w = vim.opt.columns:get()
+              local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+              local window_w = screen_w * WIDTH_RATIO
+              local window_h = screen_h * HEIGHT_RATIO
+              local window_w_int = math.floor(window_w)
+              local window_h_int = math.floor(window_h)
+              local center_x = (screen_w - window_w) / 2
+              local center_y = ((vim.opt.lines:get() - window_h) / 2)
+              - vim.opt.cmdheight:get()
+              return {
+                border = "rounded",
+                relative = "editor",
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+              }
+            end,
+          },
+          width = function()
+            return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+          end,
+        },
+      })
+      vim.keymap.set('n', '<Leader>e', require('nvim-tree.api').tree.toggle)
     end,
   }
-  use 'lambdalisue/fern-hijack.vim'
   use { 'lewis6991/gitsigns.nvim',
     event = { 'VimEnter' },
     config = function()
